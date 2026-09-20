b4de73d185d57f2f3fcee9fbcd9b7712ef000b3a
Readygoal <ahmetbasal@gmail.com>
2026-09-20 19:22:51 +0300

    Refresh cache for actionable Workbench v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='academic-writing-workbench-v5';
+const CACHE='academic-writing-workbench-v6';
 const FILES=['./','index.html','assets/styles.css','assets/main.js','assets/config.js','assets/diagnostics.js','assets/source_tools.js','assets/tools.js','assets/problems_1.js','assets/problems_2.js','assets/problems_3.js','assets/problems_4.js','manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
